96f680646df75ac62940f11df6cc3c0c51a6797e
Harshit Raj <harshit202508@gmail.com>
2026-06-23 17:26:51 +0530

    Update enhancement.py

--- enhancement.py
+++ enhancement.py
@@ -1,25 +1,25 @@
 import cv2
+import numpy as np
 import os
 
-def enhance_image(image_path):
+def enhance_image(uploaded_file):
 
-    # Read image safely
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    # Convert Streamlit file to OpenCV image
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
 
     if img is None:
-        raise ValueError("Image could not be loaded. Check file path or upload.")
+        raise ValueError("Invalid image uploaded")
 
-    # CLAHE enhancement
+    # CLAHE
     clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
     enhanced = clahe.apply(img)
 
     # Denoising
     enhanced = cv2.fastNlMeansDenoising(enhanced, None, 10, 7, 21)
 
-    # Ensure output folder exists
     os.makedirs("outputs", exist_ok=True)
-
-    output_path = os.path.join("outputs", "enhanced.jpg")
+    output_path = "outputs/enhanced.jpg"
 
     cv2.imwrite(output_path, enhanced)
 
