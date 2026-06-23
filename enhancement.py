48406376a2cfb3cb5258e5ebc41ab5043b37fd07
Harshit Raj <harshit202508@gmail.com>
2026-06-23 17:28:47 +0530

    Update enhancement.py

--- enhancement.py
+++ enhancement.py
@@ -3,24 +3,24 @@ import numpy as np
 import os
 
 def enhance_image(uploaded_file):
-
-    # Convert Streamlit file to OpenCV image
+    # Convert Streamlit uploaded file to OpenCV format
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
 
     if img is None:
-        raise ValueError("Invalid image uploaded")
+        raise ValueError("Invalid image. Please upload a valid file.")
 
-    # CLAHE
+    # CLAHE (contrast enhancement)
     clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
     enhanced = clahe.apply(img)
 
     # Denoising
     enhanced = cv2.fastNlMeansDenoising(enhanced, None, 10, 7, 21)
 
+    # Create output folder if not exists
     os.makedirs("outputs", exist_ok=True)
-    output_path = "outputs/enhanced.jpg"
 
+    output_path = "outputs/enhanced.jpg"
     cv2.imwrite(output_path, enhanced)
 
     return output_path
